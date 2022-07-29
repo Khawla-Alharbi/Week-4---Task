dd1ca1b82a7254516a63bbb2ad39fc85ef5efdd1
Khawla-Alharbi <107233141+Khawla-Alharbi@users.noreply.github.com>
2022-07-29 03:15:47 +0300

    Add files via upload

---
+++ get.php
@@ -0,0 +1,53 @@
+
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+    <head>
+<meta charset="utf-8">
+<title>Sensors</title>
+<link rel="stylesheet" href="style.css" />
+
+    </head>
+    <body>
+        <header> <h1>أرسل قيمة الحساسات</h1>  </header>
+        <hr>
+        <p>قيمة الحساس:</p>
+        
+       
+        <form >
+            <input type = "number" name = "ss" />
+            <input type="submit" >
+       
+        <p id ="done"></p>
+    </form>
+      
+    </body>
+   
+</html>
+<?php
+if($_GET["ss"]){
+$SData=  $_GET["ss"];
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sensors";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "UPDATE sensor_data SET data='$SData' WHERE id=1";
+
+if ($conn->query($sql) === TRUE) {
+    $s="yes";
+  } else {
+    echo "Error updating record: " . $conn->error;
+  }
+$conn->close();
+
+}
+?>
